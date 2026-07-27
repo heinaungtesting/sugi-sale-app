56bd437520114349d87e388f1985537c5ba9de5e
Hein Htet Aung <heinaungtesting@users.noreply.github.com>
2026-07-27 07:04:36 +0000

    fix: refresh installed PWA after haptic release

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'sugi-pwa-v21';
+const CACHE_VERSION = 'sugi-pwa-v22';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const PAGE_CACHE = `${CACHE_VERSION}-pages`;
 const OFFLINE_URL = '/offline';
